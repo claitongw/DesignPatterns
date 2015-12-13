1fea408dc4f09f0d7a5b7fd177636d943de771fd
Claiton Watthier <cla.cgw@gmail.com>
2015-12-13 16:05:18 -0200

    a

--- DesignPatterns/src/singleton/App.java
+++ DesignPatterns/src/singleton/App.java
@@ -4,6 +4,11 @@ public class App {
 
 	public static void main(String[] args) {
 
+		SecurityManager sm1 = SecurityManager.getInstance();
+		sm1.Login();
+		
+		SecurityManager sm2 = SecurityManager.getInstance();
+		sm2.Login();
 	}
 
 }
--- DesignPatterns/src/singleton/SecurityManager.java
+++ DesignPatterns/src/singleton/SecurityManager.java
@@ -1,7 +1,19 @@
 package singleton;
 
 public class SecurityManager {
-
+	
+	private static SecurityManager instance;
+	
+	private SecurityManager(){
+	}
+	
+	public static SecurityManager getInstance(){
+		if (instance==null){
+			instance = new SecurityManager();
+		}
+		return instance;
+	}
+	
 	public void Login(){
 		
 	}
